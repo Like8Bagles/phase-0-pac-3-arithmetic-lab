01d9e49ab3017253dbaf8ddfea463675b67110f3
Anthony <anthonykmbanks@gmail.com>
2021-03-06 15:20:44 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,47 @@
+function add(number = 5) {
+    number += 3;
+}
+
+function subtract(number = 5) {
+    number -= 3;
+}
+
+function multiply(number = 5) {
+    number *= 3;
+}
+
+function divide(number = 5) {
+    number /= 3;
+}
+
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a -= b
+}
+
+function multiply(a, b) {
+    return a *= b
+}
+
+function divide(a, b) {
+    return a /= b
+}
+
+function increment(n) {
+    return ++n
+}
+
+function decrement(n) {
+    return --n
+}
+
+function makeInt(n) {
+    return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
